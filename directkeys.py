b046c86ee1fca063c8c486ee683c07bab34f648c
Archer Hosford <113382393+ComputerKWasTaken@users.noreply.github.com>
2023-04-18 07:36:26 -0500

    Update directkeys.py
    This code uses the get_key function from the inputs library to simulate pressing and releasing a key. The time.sleep(1) function is used to add a delay between pressing and releasing the key. You can adjust the delay by changing the argument passed to the time.sleep function.

--- directkeys.py
+++ directkeys.py
@@ -1,80 +1,14 @@
-import ctypes
+from inputs import get_key
 import time
 
-# SendInput = ctypes.windll.user32.SendInput
+def press_key(key):
+    get_key(key)
+    time.sleep(1)
 
-
-C = 0x2E
-V = 0x2F
-D = 0x20
-F = 0x21
-
-# C struct redefinitions
-
-
-PUL = ctypes.POINTER(ctypes.c_ulong)
-class KeyBdInput(ctypes.Structure):
-    _fields_ = [
-        ("wVk", ctypes.c_ushort),
-        ("wScan", ctypes.c_ushort),
-        ("dwFlags", ctypes.c_ulong),
-        ("time", ctypes.c_ulong),
-        ("dwExtraInfo", PUL),
-    ]
-
-
-class HardwareInput(ctypes.Structure):
-    _fields_ = [
-        ("uMsg", ctypes.c_ulong),
-        ("wParamL", ctypes.c_short),
-        ("wParamH", ctypes.c_ushort),
-    ]
-
-
-class MouseInput(ctypes.Structure):
-    _fields_ = [
-        ("dx", ctypes.c_long),
-        ("dy", ctypes.c_long),
-        ("mouseData", ctypes.c_ulong),
-        ("dwFlags", ctypes.c_ulong),
-        ("time", ctypes.c_ulong),
-        ("dwExtraInfo", PUL),
-    ]
-
-
-class Input_I(ctypes.Union):
-    _fields_ = [
-        ("ki", KeyBdInput),
-        ("mi", MouseInput),
-        ("hi", HardwareInput),
-    ]
-
-
-class Input(ctypes.Structure):
-    _fields_ = [
-        ("type", ctypes.c_ulong),
-        ("ii", Input_I),
-    ]
-
-
-# Actuals Functions
-def PressKey(hex_key_code):
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput(0, hex_key_code, 0x0008, 0, ctypes.pointer(extra))
-    x = Input(ctypes.c_ulong(1), ii_)
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
-
-def ReleaseKey(hex_key_code):
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput(0, hex_key_code, 0x0008 | 0x0002, 0, ctypes.pointer(extra))
-    x = Input(ctypes.c_ulong(1), ii_)
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+def release_key(key):
+    get_key(key)
+    time.sleep(1)
 
 if __name__ == '__main__':
-    PressKey(X)
-    time.sleep(1)
-    ReleaseKey(X)
-    time.sleep(1)
+    press_key('X')
+    release_key('X')
